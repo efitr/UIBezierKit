0f95f92f725c36287e2dff3cb2ee492c284ea3e1
Egon Fiedler <efiedler93@gmail.com>
2019-03-20 16:55:08 -0700

    This is meant for the color and custom traits of the shape

---
+++ UIBezierKit/Properties.swift
@@ -0,0 +1,31 @@
+//
+//  Properties.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/20/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class Properties: UIView {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // This should make it easy to setup the properties
+    ////////////////////////////////////////////////////////////////////////////
+    
+    // What are the properties:
+    //  * The color
+    
+    var path: UIBezierPath!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.darkGray
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
