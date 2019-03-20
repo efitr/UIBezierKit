637695c2e79fa01f819ce0f14c95985ddfa4405d
Egon Fiedler <efiedler93@gmail.com>
2019-03-20 16:07:02 -0700

    starting the class

---
+++ UIBezierKit/UIBasicGeometricShapes.swift
@@ -0,0 +1,52 @@
+//
+//  UIBasicGeometricShapes.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/20/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class UIBasicGeometricShapes: UIView {
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // PERFECT SHAPES
+    ////////////////////////////////////////////////////////////////////////////
+
+    // Here we are getting in all the basic geometric shapes
+    
+    // Make an square, think that you have UIView in the class itself
+    // What are the square properties
+    func square() -> UIBezierPath {
+        let bezierPath = UIBezierPath()
+        return bezierPath
+    }
+
+    // This is meant to be just the math to make this
+    // What are the circle properties
+    func circle() -> UIBezierPath {
+        let bezierPath = UIBezierPath()
+        return bezierPath
+    }
+    
+    func triangle() -> UIBezierPath {
+        let bezierPath = UIBezierPath()
+        return bezierPath
+    }
+    
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Not PERFECT SHAPES
+    ////////////////////////////////////////////////////////////////////////////
+    
+    // Here you get 
+    func rectangle() -> UIBezierPath {
+        let bezierPath = UIBezierPath()
+        return bezierPath
+    }
+    
+
+    
+    
+}
