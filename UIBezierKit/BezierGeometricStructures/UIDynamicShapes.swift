47baa959810b760e62cf0a7ab6c8c0e2f6ddb9d1
Egon Fiedler <efiedler93@gmail.com>
2019-03-21 12:08:17 -0700

    Next step after the first iteration in perfect shapes

--- UIBezierKit/BezierGeometricStructures/UIDynamicShapes.swift
+++ UIBezierKit/BezierGeometricStructures/UIDynamicShapes.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class UIDynamicShapes: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Not PERFECT SHAPES
+    ////////////////////////////////////////////////////////////////////////////
+    
+    // Think in steps,
+    
+    // Here you get
+    func fourLineShape() -> UIBezierPath {
+        let bezierPath = UIBezierPath()
+        return bezierPath
     }
-    */
-
+    
+    
 }
