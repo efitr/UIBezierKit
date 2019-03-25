fcafa02cacfa5aab3fa343bde9dc9807c04b625e
Egon Fiedler <efiedler93@gmail.com>
2019-03-25 10:29:43 -0700

    variables for ease at initializing the shape

---
+++ UIBezierKit/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtensions.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/25/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import _SwiftUIKitOverlayShims
+
+extension UIViewController {
+    
+}
