1a3abdaeb44b6486e55894d9721c950f1555be46
Egon Fiedler <efiedler93@gmail.com>
2019-03-24 19:55:29 -0700

    This will be the first shape construction class, where this project begins

--- UIBezierKit/BezierGeometricStructures/UIBezierGeometricPerfectStructures.swift
+++ UIBezierKit/BezierGeometricStructures/UIBezierGeometricPerfectStructures.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import UIKit
+import _SwiftUIKitOverlayShims
+
+
+class UIBezierGeometricPerfectStructures: UIBezierPath {
+    
+    // This is a non optional type? -> What does this mean?
+    
+    let bezierPath = UIBezierPath()
+    
+    /*
+     * This function should take in what? for it to work, the elements needed could be loadad
+     * using the
+     */
+    func square() {
+        //        let bezierPath = UIBezierPath()
+        
+        /* Here I have to make the Geometric shape only using the elements available for
+         the UIBezierPath, maybe in shape layer I can
+         
+         */
+        //        // Specify the point that the path should start get drawn.
+        //        bezierPath.move(to: CGPoint(x: 0.0, y: 0.0))
+        //
+        //        // Create a line between the starting point and the bottom-left side of the view.
+        //        bezierPath.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        //
+        //        // Create the bottom line (bottom-left to bottom-right).
+        //        bezierPath.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        //
+        //        // Create the vertical line from the bottom-right to the top-right side.
+        //        bezierPath.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
+        //
+        // Close the path. This will create the last line automatically.
+        
+        
+        bezierPath.close()
+        //        return bezierPath
+    }
+}
