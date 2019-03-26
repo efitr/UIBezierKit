9fafa73a0e5ba19c4298e2e0faa7bc9260565981
Egon Fiedler <efiedler93@gmail.com>
2019-03-26 08:27:16 -0700

    Setting up extension for framing different UIViews on the screen

--- UIBezierKit/Extensions/UIViewControllerExtensions.swift
+++ UIBezierKit/Extensions/UIViewControllerExtensions.swift
@@ -12,4 +12,14 @@ import _SwiftUIKitOverlayShims
 
 extension UIViewController {
     
+    /*
+     ON the UIViewController, the most we get to work on is the view: UIView!, that is
+     in here
+     We can make the elements for when you want to build directly on the screen,
+     exactly on the instance of view: UIView embedded on UIViewController
+     We can position the new view according to the view that is already here, we
+     make points out of the dimensions of this view
+     Here is for usbviews arrangement on the lower hierarchy view
+    **/
+    
 }
