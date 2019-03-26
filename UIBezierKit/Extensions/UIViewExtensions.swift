e840352e1d2282d5cfd50abf9fc8a2f6be7c00a3
Egon Fiedler <efiedler93@gmail.com>
2019-03-26 08:04:03 -0700

    Setting up approach towards working with the elements on a view

---
+++ UIBezierKit/Extensions/UIViewExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewExtensions.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/25/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import _SwiftUIKitOverlayShims
+
+extension UIView{
+    /*
+      Here is about getting elements to be arranged in the view itself
+      For example is multiple 
+     */
+}
