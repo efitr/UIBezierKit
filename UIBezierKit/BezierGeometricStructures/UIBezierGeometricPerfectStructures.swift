8700ec299c0dbf2a9c67d94f2376487262e70d7d
Egon Fiedler <efiedler93@gmail.com>
2019-03-26 23:19:14 -0700

    update of a not usefull class

--- UIBezierKit/BezierGeometricStructures/UIBezierGeometricPerfectStructures.swift
+++ UIBezierKit/BezierGeometricStructures/UIBezierGeometricPerfectStructures.swift
@@ -32,36 +32,33 @@ class UIBezierGeometricPerfectStructures: UIBezierPath {
      * This function should take in the position of the square
      */
     func square() {
-        //        let bezierPath = UIBezierPath()
-        
         /* Here I have to make the Geometric shape only using the elements available for
-         the UIBezierPath, maybe in shape layer I can
-         
+         the UIBezierPath.
          */
-        //        // Specify the point that the path should start get drawn.
-        //        bezierPath.move(to: CGPoint(x: 0.0, y: 0.0))
-        //
-        //        // Create a line between the starting point and the bottom-left side of the view.
-        //        bezierPath.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
-        //
-        //        // Create the bottom line (bottom-left to bottom-right).
-        //        bezierPath.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
-        //
-        //        // Create the vertical line from the bottom-right to the top-right side.
-        //        bezierPath.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
-        //
+        
+        bezierPath = UIBezierPath()
+        
+        // Specify the point that the path should start get drawn.
+        bezierPath.move(to: CGPoint(x: 0.0, y: 0.0))
+
+        // Create a line between the starting point and the bottom-left side of the view.
+        bezierPath.addLine(to: CGPoint(x: 0.0, y: self.bounds.size.height))
+
+        // Create the bottom line (bottom-left to bottom-right).
+        bezierPath.addLine(to: CGPoint(x: self.bounds.size.width, y: self.bounds.size.height))
+
+        // Create the vertical line from the bottom-right to the top-right side.
+        bezierPath.addLine(to: CGPoint(x: self.bounds.size.width, y: 0.0))
+
         // Close the path. This will create the last line automatically.
-        
-        
         bezierPath.close()
-        //        return bezierPath
     }
     
     func triangle() {
-        let bezierPath = UIBezierPath()
-//        bezierPath.move(to: CGPoint(x: UInt32, y: UInt32))
-//        bezierPath.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
-//        bezierPath.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        bezierPath = UIBezierPath()
+        bezierPath.move(to: CGPoint(x: self.bounds.width/2, y: 0.0))
+        bezierPath.addLine(to: CGPoint(x: 0.0, y: self.bounds.size.height))
+        bezierPath.addLine(to: CGPoint(x: self.bounds.size.width, y: self.bounds.size.height))
         bezierPath.close()
     }
 }
