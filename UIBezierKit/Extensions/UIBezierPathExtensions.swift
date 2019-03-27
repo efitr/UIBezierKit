62a5052b74d786d3e1c5d1ffef3cd3343bf4bf63
Egon Fiedler <efiedler93@gmail.com>
2019-03-26 23:48:12 -0700

    extension for ease of making shapes

---
+++ UIBezierKit/Extensions/UIBezierPathExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIBezierPathExtensions.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/26/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import _SwiftUIKitOverlayShims
+
+extension UIBezierPath {
+    
+}
