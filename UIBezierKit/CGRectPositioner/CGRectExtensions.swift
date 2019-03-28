2326cc6be2d22d0c15d33845ba317a000051bee0
Egon Fiedler <efiedler93@gmail.com>
2019-03-28 16:54:39 -0700

    reshaped the library

---
+++ UIBezierKit/CGRectPositioner/CGRectExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  CGRectExtensions.swift
+//  UIBezierKit
+//
+//  Created by Egon Fiedler on 3/21/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+/* Each class has features
+    * What functionality are both addind that make the CoreGraphis element work
+    * Do I need to have both been called here?
+*/
+
+import CoreGraphics
+import Darwin
+
+extension CGRect {
+
+}
--- UIBezierKit/ViewController.swift
+++ UIBezierKit/ViewController.swift
@@ -25,26 +25,26 @@ class ViewController: UIViewController {
         //What exactly is this doing?
         super.viewDidAppear(animated)
         
-        // This is about the shape size
-        let width: CGFloat = 240.0
-        let height: CGFloat = 160.0
-        
-        /* Here is setting up the size and position
-         * of the whole image, this happens because of the initializer
-         */
-        let basicGeometricShapes = UIBasicGeometricShapes(
-            frame: CGRect(
-            x:
-            /* This is framing */
-            self.view.frame.size.width/2 - width/2,
-            y:
-            
-            self.view.frame.size.height/2 - height/2,
-            width: width,
-            height: height))
-        // Given that DemoView inherits from a UIView it can be used by the property addSubView
-        // view is part UIViewController
-        self.view.addSubview(basicGeometricShapes)
+//        // This is about the shape size
+//        let width: CGFloat = 240.0
+//        let height: CGFloat = 160.0
+//        
+//        /* Here is setting up the size and position
+//         * of the whole image, this happens because of the initializer
+//         */
+//        let basicGeometricShapes = UIBasicGeometricShapes(
+//            frame: CGRect(
+//            x:
+//            /* This is framing */
+//            self.view.frame.size.width/2 - width/2,
+//            y:
+//            
+//            self.view.frame.size.height/2 - height/2,
+//            width: width,
+//            height: height))
+//        // Given that DemoView inherits from a UIView it can be used by the property addSubView
+//        // view is part UIViewController
+//        self.view.addSubview(basicGeometricShapes)
     }
     
     // Second step 
